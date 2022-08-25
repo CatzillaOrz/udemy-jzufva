55fb3315687badbe608dc18ad1a7a8fa936d4528
catzillaorz <catzillaorz@outlook.com>
2022-08-25 16:25:55 +0800

    feat: fetch recip

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,7 +3,7 @@ const recipeContainer = document.querySelector('.recipe');
 const timeout = function (s) {
   return new Promise(function (_, reject) {
     setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
+      reject(new Error(`Request took too long! ! Timeout after ${s} second`));
     }, s * 1000);
   });
 };
@@ -11,3 +11,22 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+/**
+ *  // https://forkify-api.herokuapp.com/api/v2/recipes/:id
+ * feth Recipe
+ */
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+showRecipe();
