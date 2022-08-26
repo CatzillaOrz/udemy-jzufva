d6157d8d2561bf41b2834a64226042cd43dc9d3f
catzillaorz <catzillaorz@outlook.com>
2022-08-26 15:56:39 +0800

    feat: add hash id & window event listen

--- src/js/controller.js
+++ src/js/controller.js
@@ -33,8 +33,10 @@ const renderSpinner = function (parentEl) {
 const showRecipe = async function () {
   try {
     renderSpinner(recipeContainer);
+    const id = window.location.hash.slice(1);
+    if (!id) return;
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
     console.log(data);
@@ -155,4 +157,4 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
