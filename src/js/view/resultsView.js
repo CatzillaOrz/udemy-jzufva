fc2716b19b925f2b964472e916cb56c632ea1486
catzillaorz <catzillaorz@outlook.com>
2022-08-30 16:36:36 +0800

    feat: results error message

--- src/js/view/resultsView.js
+++ src/js/view/resultsView.js
@@ -2,6 +2,9 @@ import View from './view';
 import icons from 'url:../../img/icons.svg';
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for you query! Please try again!';
+  _message = '';
+
   __generateMarkup() {
     return `
       <li class="preview">
