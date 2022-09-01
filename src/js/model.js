0ebabc2b9d6c607086c4c377be6684db2ba73839
catzillaorz <catzillaorz@outlook.com>
2022-09-01 16:24:16 +0800

    feat: update addHandlerRender of bookmarksView

--- src/js/model.js
+++ src/js/model.js
@@ -70,10 +70,13 @@ export const updateServing = function (newServings) {
   });
   state.recipe.servings = newServings;
 };
-
+export const persistBookmarks = function () {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
 export const addBookmark = function (recipe) {
   state.bookmarks.push(recipe);
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+  persistBookmarks();
 };
 
 export const deleteBookmark = function (id) {
@@ -81,4 +84,12 @@ export const deleteBookmark = function (id) {
   state.bookmarks.splice(index, 1);
 
   if (id === state.recipe.id) state.recipe.bookmarked = false;
+  persistBookmarks();
 };
+
+const init = function () {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
+
+init();
