9e88993214937622fcf14379419a92efa8dc0973
catzillaorz <catzillaorz@outlook.com>
2022-09-01 15:15:36 +0800

    feat: bookmarks previewView

--- src/js/view/bookmarksView.js
+++ src/js/view/bookmarksView.js
@@ -1,5 +1,6 @@
 import View from './view';
 import icons from 'url:../../img/icons.svg';
+import previewView from './previewView';
 class BookmarksView extends View {
   _parentElement = document.querySelector('.bookmarks__list');
   _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it;)';
@@ -7,28 +8,8 @@ class BookmarksView extends View {
 
   _generateMarkup() {
     return this._data
-      .map(result => this._generateMarkupPreview(result))
+      .map(bookmark => previewView.render(bookmark, false))
       .join('');
-  }
-  _generateMarkupPreview(result) {
-    const id = window.location.hash.slice(1);
-    return `
-    <li class="preview">
-      <a class="preview__link ${
-        id === result.id ? 'preview__link--active' : ''
-      }"  href="#${result.id}">
-        <figure class="preview__fig">
-          <img src="${result.image}" alt="${result.title}" />
-        </figure>
-        <div class="preview__data">
-          <h4 class="preview__title">${result.title}</h4>
-          <p class="preview__publisher">${result.publisher}</p>
-        </div>
-      </a>
-    </li>
-
-
-    `;
   }
 }
 
